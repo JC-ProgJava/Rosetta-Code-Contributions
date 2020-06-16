569eb787a18ab456b06d355d869e90c00e667044
JC-ProgJava <juch5796@gmail.com>
2020-06-16 14:15:22 +0800

    Updated versions, uploaded to Github.

---
+++ HelloWorld.java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[]args){
+        HelloWorld hw = new HelloWorld();
+        hw.run();
+    }
+    void run(){
+        System.out.println("Hello world!");
+    }
+}
